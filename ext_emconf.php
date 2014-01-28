dd5d6af8bdfb5f45057046f6bf2425daac56c6ee
roboterfreak <roboterfreak@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-01-28 19:31:43 +0000

    initial commit to repo of EXT version 0.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jh_smilie_viewhelper/trunk@83426 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "jh_smilie_viewhelper".
+ *
+ * Auto generated 28-01-2014 18:39
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'Smilie Viewhelper',
+	'description' => 'This extensions provides two fluid viewhelpers for EXT:smilie',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '0.0.3',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => NULL,
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jonathan Heilmann',
+	'author_email' => 'mail@jonathan-heilmann.de',
+	'author_company' => '',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' =>
+	array (
+		'depends' =>
+		array (
+			'typo3' => '4.5.0-6.1.99',
+			'smilie' => '',
+		),
+		'conflicts' =>
+		array (
+		),
+		'suggests' =>
+		array (
+		),
+	),
+);
+
+?>
